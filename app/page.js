4f913ffc88ca2a10deef69ccb26ad57b23024310
Cesar Villanueva Jr <cesar.villanueva@ardentforms.com>
2024-10-28 19:48:08 -0700

    PhotoFrame Rotation
    Updated PhotoFrame component to employ a simple rotation of an array of
    photos. I added 'use client' because I'm using state but I will look
    into breaking this component further so its moved into a smaller piece.

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
 import styles from './page.module.scss';
 import PhotoFrame from '@/components/photo-frame/PhotoFrame';
 
+const localPhotos = [
+	'/images/test_000.jpg',
+	'/images/test_001.jpg',
+	'/images/test_002.jpg',
+	'/images/test_003.jpg'
+];
+
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<PhotoFrame imageURL="/images/test.jpg" />
+			<PhotoFrame photos={localPhotos} />
 		</main>
 	);
 }
--- components/photo-frame/PhotoFrame.js
+++ components/photo-frame/PhotoFrame.js
@@ -1,47 +1,56 @@
+'use client';
+
 import styles from './PhotoFrame.module.scss';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
-const PhotoFrame = (props) => {
+const PhotoFrame = ({ photos }) => {
+	const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentPhotoIndex(
+				(prevIndex) => (prevIndex + 1) % photos.length
+			);
+		}, 10000);
+
+		return () => clearInterval(interval);
+	}, [photos]);
+
 	return (
-		<>
-			{props.imageURL && (
-				<div className={styles['photo-frame']}>
-					{/* Next.js 13 requires fill prop if not using width and height */}
-					<Image
-						alt="An image to display..."
-						className={styles['photo-frame__image']}
-						fill
-						priority
-						src={props.imageURL}
-					/>
-					<div className={styles.widget}>
-						<time
-							className={styles.widget__date}
-							dateTime="2024-10-28T19:30:00"
-						>
-							28 October, Monday
-						</time>
-						<div className={styles.widget__temperature}>
-							76&deg;c
-						</div>
-						<Image
-							alt="Sunny day icon"
-							className={styles.widget__icon}
-							height={28}
-							src="/sunny.svg"
-							width={28}
-						/>
-						<div className={styles.widget__location}>Compton</div>
-						<time
-							className={styles.widget__time}
-							dateTime="2024-10-28T19:30:00"
-						>
-							7:30 PM
-						</time>
-					</div>
-				</div>
-			)}
-		</>
+		<div className={styles['photo-frame']}>
+			{/* Next.js 13 requires fill prop if not using width and height */}
+			<Image
+				alt="Displaying a photo..."
+				className={styles['photo-frame__image']}
+				fill
+				priority
+				src={photos[currentPhotoIndex]}
+			/>
+			<div className={styles.widget}>
+				<time
+					className={styles.widget__date}
+					dateTime="2024-10-28T19:30:00"
+				>
+					28 October, Monday
+				</time>
+				<div className={styles.widget__temperature}>76&deg;c</div>
+				<Image
+					alt="Sunny day icon"
+					className={styles.widget__icon}
+					height={28}
+					src="/sunny.svg"
+					width={28}
+				/>
+				<div className={styles.widget__location}>Compton</div>
+				<time
+					className={styles.widget__time}
+					dateTime="2024-10-28T19:30:00"
+				>
+					7:30 PM
+				</time>
+			</div>
+		</div>
 	);
 };
 
