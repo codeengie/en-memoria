6c005fa9fbc196a30c509816930555e12734c4d7
Cesar Villanueva Jr <cesar.villanueva@ardentforms.com>
2024-11-02 22:53:06 -0700

    Removed Photos
    + Removed photos folder from 1gitignore1
    + Removed `1localPhotos1` array from page.js

--- app/page.js
+++ app/page.js
@@ -1,19 +1,10 @@
 import styles from './page.module.scss';
 import PhotoFrame from '@/components/photo-frame/PhotoFrame';
 
-const localPhotos = [
-	'/images/test_000.jpg',
-	'/images/test_001.jpg',
-	'/images/test_002.jpg',
-	'/images/test_003.jpg',
-	'/images/test_004.jpg',
-	'/images/test_005.jpg'
-];
-
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<PhotoFrame photos={localPhotos} />
+			<PhotoFrame />
 		</main>
 	);
 }
